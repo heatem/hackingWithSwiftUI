657ac2054e1dd2c39db78ac05902a8e94a18230a
Heather Mason <heatem@gmail.com>
2020-02-27 23:41:15 -0500

    fix delete, customize input

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -10,26 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items, id: \.name) { item in
-                    Text(item.name)
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text("$\(item.amount)")
+                    }
                 }
             .onDelete(perform: removeItems)
             }
             .navigationBarTitle("iExpense")
             .navigationBarItems(trailing:
                     Button(action: {
-                        let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                        self.expenses.items.append(expense)
+                        self.showingAddExpense = true
+                        
                     }) {
                         Image(systemName: "plus")
                 }
             )
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: self.expenses)
+            }
         }
     }
+    
     func removeItems(at offsets: IndexSet) {
         expenses.items.remove(atOffsets: offsets)
     }
